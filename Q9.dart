c484198e82d046ca1c821fd5c89b04c83f76b820
connectalizain <connectalizain@gmail.com>
2023-02-24 18:31:39 +0500

    Q8 to Q10

---
+++ Q9.dart
@@ -0,0 +1,8 @@
+void main() {
+  var world = {
+    'Pakistan': {'Capital': 'Islamabad', 'Language': 'Urdu', 'Currency': 'PKR'},
+    'India': {'Capital': 'Delhi', 'Language': 'Hindi', 'Currency': 'INR'},
+    'USA': {'Capital': 'Washington', 'Language': 'English', 'Currency': 'USD'},
+  };
+  print(world['Pakistan']);
+}
